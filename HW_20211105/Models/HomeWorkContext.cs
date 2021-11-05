2bc0e2577e4e7de50a38639f488211175f8337e6
Yuu <yuu.lin@shinda.com.tw>
2021-11-05 20:33:04 +0800

    dbcontext

---
+++ HW_20211105/Models/HomeWorkContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace HW_20211105.Models
+{
+    public partial class HomeWorkContext : DbContext
+    {
+        public HomeWorkContext()
+        {
+        }
+
+        public HomeWorkContext(DbContextOptions<HomeWorkContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblDepartment> TblDepartment { get; set; }
+        public virtual DbSet<TblStaff> TblStaff { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblDepartment>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblStaff>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Title)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ HW_20211105/Models/TblStaff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace HW_20211105.Models
+{
+    public partial class TblStaff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Department { get; set; }
+        public string Title { get; set; }
+        public int? Age { get; set; }
+    }
+}
